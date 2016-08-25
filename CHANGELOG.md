ceeb73fb6fbc43f9477d58d49dae3200a61636f7
Ramon Tayag <ramon.tayag@gmail.com>
2016-08-25 12:51:11 +0800

    Require bank_code, branch_name, account_no only if bank transaction

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,7 @@ This project adheres to [Semantic Versioning](http://semver.org/).
 ## [Unreleased]
 ### Changed
 - Update `trans_date` to new required format (`2016-08-23 15:30:05 +0800`)
+- Require `bank_code`, `branch_name`, `account_no` only if bank transaction
 
 ## [0.1.0] - 2016-08-22
 ### Added
--- lib/ezapi_client/schemas/create_transaction_request_schema.rb
+++ lib/ezapi_client/schemas/create_transaction_request_schema.rb
@@ -1,6 +1,8 @@
 module EZAPIClient
   CreateTransactionRequestSchema = Dry::Validation.Schema(BaseSchema) do
 
+    BANK_TRANS_TYPE = "CBA"
+
     required(:reference_no).filled(:str?, max_size?: 20)
     required(:trans_date).filled(:date_time?)
     required(:sender_lastname).filled(:str?, max_size?: 40)
@@ -21,9 +23,9 @@ module EZAPIClient
       max_size?: 3,
       included_in?: CreateTransactionRequest::TRANS_TYPES.keys,
     })
-    required(:bank_code).filled(:str?, max_size?: 5)
-    required(:branch_name).filled(:str?, max_size?: 40)
-    required(:account_no).filled(:str?, max_size?: 20)
+    required(:bank_code).maybe(:str?, max_size?: 5)
+    required(:branch_name).maybe(:str?, max_size?: 40)
+    required(:account_no).maybe(:str?, max_size?: 20)
     required(:landed_currency).filled(:str?, {
       max_size?: 3,
       included_in?: %w(USD PHP),
@@ -38,6 +40,22 @@ module EZAPIClient
       trans_types = CreateTransactionRequest::\
         TRANS_TYPE_CATEGORIES[:door_to_door]
       trans_type.included_in?(trans_types).then(recipient_birthday.filled?)
+    end
+
+    rule(bank_code_presence: %i[trans_type bank_code]) do |trans_type, bank_code|
+      trans_type.eql?(BANK_TRANS_TYPE).then(bank_code.filled?)
+    end
+
+    rule(
+      branch_name_presence: %i[trans_type branch_name]
+    ) do |trans_type, branch_name|
+      trans_type.eql?(BANK_TRANS_TYPE).then(branch_name.filled?)
+    end
+
+    rule(
+      account_no_presence: %i[trans_type account_no]
+    ) do |trans_type, account_no|
+      trans_type.eql?(BANK_TRANS_TYPE).then(account_no.filled?)
     end
 
     rule(
